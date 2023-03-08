8af977d79320d82e6524db3e212328d6c590abb1
cos44162 <sacha.colucci@st.oth-regensburg.de>
2023-03-08 12:18:04 +0100

    addes live update sdk

---
+++ android/app/src/main/java/com/colsach/propabilitypal/MainActivity.java
@@ -0,0 +1,5 @@
+package com.colsach.propabilitypal;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.colsach.propabilitypal',
   appName: 'ProbabilityPal',
   webDir: 'www',
   bundledWebRuntime: false
